00ebdcf30338338b1a08d8c7a9e4dbef57400a4f
butterglazed-paulomon <butterglazed@gmail.com>
2021-04-21 08:25:15 +0800

    added HEN213b

--- 75XV2/webkit/ps4.js
+++ 75XV2/webkit/ps4.js
@@ -212,6 +212,9 @@ function toggle_payload(pld){
 	}else if(pld == "webrte"){
 		document.getElementById("progress").innerHTML="Loading Payload.. Please wait..";
 		preloadScripts(['payloads/preloader.js', 'payloads/webrte.js', 'payloads/loader.js']);
+	}else if(pld == "hen213b"){
+		document.getElementById("progress").innerHTML="Loading HEN213b.. Please wait..";
+		preloadScripts(['payloads/preloader.js', 'payloads/hen213b.js', 'payloads/loader.js']);
 	}
 	if(window.postPayload)
 		window.postPayload();
