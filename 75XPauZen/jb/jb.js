bae3690a5ba8346a9352acaf6004b774d769029b
butterglazed-paulomon <butterglazed@gmail.com>
2021-05-04 15:57:14 +0800

    added confirmation on jb.js

--- 75XPauZen/jb/jb.js
+++ 75XPauZen/jb/jb.js
@@ -150666,3 +150666,9 @@ var printf_ans = read_mem_as_string(printf_buf, printf_buf_end-printf_buf);
 var _ = malloc_nogc.pop();
 var _ = malloc_nogc.pop();
 var _ = malloc_nogc.pop();
+if (main_ret == 179 || main_ret == 0) {
+  document.getElementById("progress").innerHTML = "Paulomon Exploit Complete!";
+} else {
+  alert("Exploit Failed. Restart and Try Again");
+  document.getElementById("progress").innerHTML = "Exploit Failed. Restart and Try Again";
+}
