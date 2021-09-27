c40deaf345c9e99fa5f12aed5a3c696d7e19ee77
butterglazed-paulomon <butterglazed@gmail.com>
2021-09-27 13:20:22 +0800

    goldhen2

--- 75XPauZen/webkit/ps4.js
+++ 75XPauZen/webkit/ps4.js
@@ -214,12 +214,27 @@ function toggle_payload(pld){
 	}else if(pld == "webrte"){
 		document.getElementById("progress").innerHTML="Loading Payload.. Please wait..";
 		preloadScripts(['payloads/preloader.js', 'payloads/webrte.js', 'payloads/loader.js']);
+	}else if(pld == "goldhen20"){
+		document.getElementById("progress").innerHTML="Loading Payload.. Please wait..";
+		load_goldhen20b()
+		
+
 	}
 	if(window.postPayload)
 		window.postPayload();
 	payload_finished(pld);
 	
 }
+//inserted function from prb exploit - Paulomon
+function loadScript2(){getScript("loader.js",function(){});}
+		
+function load_goldhen20b(){
+	msgs.innerHTML='Loading GoldHen V2.0B1 ...';
+	LoadedMSG="GoldHen V2.0B1 Loaded";
+	PLfile = "755.bin"
+	loadScript2();
+}
+//end of added functions
 
 function payload_finished(payload)
 {
